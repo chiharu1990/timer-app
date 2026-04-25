bdee47e0de2a1fff5d8761be50d9e54fab76b92a
chiharu1990 <chiharufukuyama.1990@gmail.com>
2026-04-25 21:25:29 +0900

    update：タイマーの実装

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 let remainingTime = 300;
+let timerIntervalId = null;
 
-document.getElementById("start-btn").addEventListener("click", () => {
-  console.log("スタート！")
+// タイマーの関数
+const timer = () => {
+  if (timerIntervalId !== null) return;
+  timerIntervalId = setInterval(countdown, 1000);
+}
+
+// タイマーの形式でカウントダウンする関数
+const countdown = () => {
   if (remainingTime > 0){
     remainingTime -= 1;
     const minutes = Math.floor(remainingTime / 60);
     const seconds = remainingTime % 60;
-    document.getElementById("timer-display").textContent = `${minutes}:${String(seconds).padStart(2, "0")}`;
+    document.getElementById("timer-display").textContent = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+  } else {
+    clearInterval(timerIntervalId);
+    console.log("タイマー終了");
   }
-})
+}
+
+// イベント登録
+document.getElementById("start-btn").addEventListener("click", timer);
